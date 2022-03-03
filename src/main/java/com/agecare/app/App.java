83cd87bd515b06fd9aa2637dee03d0c4c2916341
rhodricgana <rhodricgana@gmail.com>
2022-03-03 15:23:18 +0000

    my first maven

---
+++ src/main/java/com/agecare/app/App.java
@@ -0,0 +1,13 @@
+package com.agecare.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World- I love you Callum, Eliana, Kara!" );
+    }
+}
